a71846bf5a620a53851c32747fe6ddab2dbbbc95
Karolina Pereira <karolina.pereira.2331@gmail.com>
2025-04-15 11:08:51 -0300

    learn(ap16): encerra a apostila

--- ap16_cursores/script.sql
+++ ap16_cursores/script.sql
@@ -13,7 +13,7 @@
 -- ALTER COLUMN video_views TYPE BIGINT;
 -- SELECT * FROM tb_top_youtubers;
 
--- MEXENDO NO PRIMEIRO CURSOR
+-- MEXENDO NO PRIMEIRO CURSOR - CURSOR NÃO VINCULADO
 DO $$
 DECLARE
 --1. declaração do cursor
@@ -36,3 +36,130 @@ OPEN cur_nomes_youtubers FOR
 CLOSE cur_nomes_youtubers;
 END;
 $$
+
+-- CURSOR NÃO VINCULADO COM QUERY DINAMICA (em tempo de execução vc decide qual comando executar - ex. quando há opções)
+-- exibir os nomes dos youtubers que começaram a partir de um ano específico 
+-- os cursores são melhores em questão de desempenho por não pegar toda a tabela, e sim, uma parte especifica
+DO $$
+DECLARE
+	cur_nomes_a_partir_de REFCURSOR;
+	v_youtuber VARCHAR(200);
+	v_ano INT := 2008; -- serve para colocar o limite de "quero youtubers q começaram em 2008"
+	v_nome_tabela VARCHAR(200) := 'tb_top_youtubers';
+	
+BEGIN
+	-- fazer o select em função das variaveis
+	-- 2. abertura do cursor
+	OPEN cur_nomes_a_partir_de FOR EXECUTE -- usar quando a query for dinamica
+	format(
+		'SELECT youtuber 
+		FROM %s  
+		WHERE started >= $1' 
+		, v_nome_tabela
+	) 
+	USING v_ano;
+	LOOP
+		-- 3. Recuperação de dados
+		FETCH cur_nomes_a_partir_de INTO v_youtuber;
+		EXIT WHEN NOT FOUND; 
+		RAISE NOTICE '%', v_youtuber;
+	END LOOP;
+	-- 4. Fechar o cursor
+	CLOSE cur_nomes_a_partir_de;
+END;
+$$
+
+-- CURSOR VINCULADO(BOUND)
+-- concatenar nome e número de inscritos
+DO $$
+DECLARE
+	cur_nomes_e_inscritos CURSOR FOR
+	SELECT youtuber, subscribers FROM tb_top_youtubers;
+	tupla RECORD;
+	resultado TEXT DEFAULT '';
+BEGIN
+	-- 2. abertura
+	OPEN cur_nomes_e_inscritos;
+	FETCH cur_nomes_e_inscritos INTO tupla;
+	WHILE FOUND LOOP
+		resultado := resultado || tupla.youtuber || ': ' || tupla.subscribers || ',';
+		-- 3. recuperação dos dados
+		FETCH cur_nomes_e_inscritos INTO tupla;
+	END LOOP;
+	-- 4. Fechamento
+	CLOSE cur_nomes_e_inscritos;
+	RAISE NOTICE '%', resultado;
+END; 
+$$
+
+-- CURSORES COM PARAMETROS NOMEADOS E PELA ORDEM
+-- começaram a partir de 2010 e tem pelo menos 60M de inscritos 
+DO $$
+DECLARE
+	 v_ano INT := 2010;
+	 v_inscritos INT := 60_000_000;
+	 v_youtuber VARCHAR(200);
+	 -- 1. declaração
+	 cur_ano_inscritos CURSOR(ano INT, inscritos INT) 
+	 FOR SELECT youtuber 
+	 FROM tb_top_youtubers 
+	 WHERE started >= ano AND subscribers >= inscritos;
+BEGIN
+	-- 2. abertura
+	OPEN cur_ano_inscritos(
+		ano := v_ano,
+		inscritos := v_inscritos
+	);
+	LOOP 
+		-- 3. recuperação de dados
+		FETCH cur_ano_inscritos INTO v_youtuber;
+		EXIT WHEN NOT FOUND;
+		RAISE NOTICE '%', v_youtuber;
+	END LOOP;
+	-- 4. fechamento
+	CLOSE cur_ano_inscritos;
+END;
+$$
+
+
+-- UPDATE E DELETE COM RECURSORES 
+-- remover tuplas em que video_count é desconhecido
+-- exibir as tuplas remanescentes de baixo
+
+-- tipo: SELECT * FROM tb_top_youtubers WHERE video_count IS NULL
+DO $$
+DECLARE
+-- 1. Declaração 
+	 cur_delete REFCURSOR;
+	 tupla RECORD;
+
+BEGIN 
+    -- 2. abertura
+	OPEN cur_delete SCROLL FOR
+	SELECT * FROM tb_top_youtubers;
+	LOOP 
+		-- 3. Recuperação de dados
+		FETCH cur_delete INTO tupla;
+		EXIT WHEN NOT FOUND;
+			IF tupla.video_count IS NULL THEN
+				DELETE FROM tb_top_youtubers 
+				WHERE CURRENT OF cur_delete;
+			END IF;
+	END LOOP;
+
+	LOOP 
+		-- 3.1 Recuperação de dados
+		 FETCH BACKWARD FROM cur_delete INTO tupla;
+		 EXIT WHEN NOT FOUND;
+		 RAISE NOTICE '%', tupla; -- mostra todo mundo que 'sobrou', pois nao tinha ninguem para remover
+	END LOOP;
+	-- 4. fechamento
+	CLOSE cur_delete;
+END;
+$$
+
+
+
+
+
+
