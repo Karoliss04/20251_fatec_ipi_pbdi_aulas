074ab7a61767516acdae9347465ae5848f8729b4
Karolina Pereira <karolina.pereira.2331@gmail.com>
2025-05-13 11:28:58 -0300

    learn(ap11): lida com o codigo 2.10.5
    - criamos o proc
    - fazer bloquinho anonimo que coloca em execucao

--- ap11_stored_procedure/script.sql
+++ ap11_stored_procedure/script.sql
@@ -1,4 +1,4 @@
-
+-- CRIANDO 
  CREATE OR REPLACE PROCEDURE sp_ola_procedures()
  LANGUAGE plpgsql
  AS $$
@@ -7,8 +7,10 @@
  END;
  $$;
 
+-- CHAMANDO
  CALL sp_ola_procedures( );
 
+--
 CREATE OR REPLACE PROCEDURE sp_ola_usuario (p_nome VARCHAR(200))
  LANGUAGE plpgsql
  AS $$
@@ -20,6 +22,7 @@ CREATE OR REPLACE PROCEDURE sp_ola_usuario (p_nome VARCHAR(200))
 
  CALL sp_ola_usuario('Pedro');
  
+ --
 CREATE OR REPLACE PROCEDURE sp_acha_maior (IN valor1 INT, valor2 INT)
  LANGUAGE plpgsql
  AS $$
@@ -34,60 +37,65 @@ CREATE OR REPLACE PROCEDURE sp_acha_maior (IN valor1 INT, valor2 INT)
 
  CALL sp_acha_maior (2, 3);
 
+--
   DROP PROCEDURE IF EXISTS sp_acha_maior;
 
+--
    CREATE OR REPLACE PROCEDURE sp_acha_maior (OUT resultado INT, IN valor1 INT, IN valor2 INT)
  LANGUAGE plpgsql
  AS $$
  BEGIN
- CASE
- WHEN valor1 > valor2 THEN
- $1 := valor1;
- ELSE
- resultado := valor2;
- END CASE;
+    CASE
+        WHEN valor1 > valor2 THEN
+        $1 := valor1;
+        ELSE
+        resultado := valor2;
+    END CASE;
  END;
  $$
 
+--
  DO $$
  DECLARE
  resultado INT;
  BEGIN
- CALL sp_acha_maior (resultado, 2, 3);
- RAISE NOTICE '% é o maior', resultado;
+    CALL sp_acha_maior (resultado, 2, 3);
+    RAISE NOTICE '% é o maior', resultado;
  END;
  $$
 
 DROP PROCEDURE IF EXISTS sp_acha_maior;
 
+--
 CREATE OR REPLACE PROCEDURE sp_acha_maior (INOUT valor1 INT, IN valor2 INT)
  LANGUAGE plpgsql
  AS $$
  BEGIN
- IF valor2 > valor1 THEN
- valor1 := valor2;
- END IF;
+    IF valor2 > valor1 THEN
+    valor1 := valor2;
+    END IF;
  END;
  $$
 
  DO
  $$
  DECLARE
- valor1 INT := 2;
- valor2 INT := 3;
+    valor1 INT := 2;
+    valor2 INT := 3;
  BEGIN
- CALL sp_acha_maior(valor1, valor2);
- RAISE NOTICE '% é o maior', valor1;
- END;
+    CALL sp_acha_maior(valor1, valor2);
+    RAISE NOTICE '% é o maior', valor1;
+END;
  $$
 
+--
  CREATE OR REPLACE PROCEDURE sp_calcula_media ( VARIADIC  valores INT [])
  LANGUAGE plpgsql
  AS $$
  DECLARE
     media NUMERIC(10, 2) := 0;
     valor INT;
-    BEGIN
+BEGIN
     FOREACH valor IN ARRAY valores LOOP
     media := media + valor;
     END LOOP;
@@ -101,49 +109,128 @@ CREATE OR REPLACE PROCEDURE sp_acha_maior (INOUT valor1 INT, IN valor2 INT)
 
  DROP TABLE tb_cliente;
  CREATE TABLE tb_cliente (
- cod_cliente SERIAL PRIMARY KEY,
- nome VARCHAR(200) NOT NULL
+    cod_cliente SERIAL PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL
  );
-
-  CREATE TABLE IF NOT EXISTS tb_pedido(
- cod_pedido SERIAL PRIMARY KEY,
- data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- data_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- status VARCHAR DEFAULT 'aberto',
- cod_cliente INT NOT NULL,
- CONSTRAINT fk_cliente FOREIGN KEY (cod_cliente) REFERENCES
- tb_cliente(cod_cliente)
- );
+DROP TABLE IF EXISTS tb_pedido;
+CREATE TABLE IF NOT EXISTS tb_pedido(
+    cod_pedido SERIAL PRIMARY KEY,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+status VARCHAR DEFAULT 'aberto',
+cod_cliente INT NOT NULL,
+CONSTRAINT fk_cliente FOREIGN KEY (cod_cliente) REFERENCES
+tb_cliente(cod_cliente)
+);
 
 SELECT * FROM tb_pedido;
 
 CREATE TABLE tb_tipo_item(
  cod_tipo SERIAL PRIMARY KEY,
  descricao VARCHAR(200) NOT NULL
- );
+);
 
   INSERT INTO tb_tipo_item (descricao) VALUES ('Bebida'), ('Comida');
 
-  CREATE TABLE IF NOT EXISTS tb_item(
- cod_item SERIAL PRIMARY KEY,
- descricao VARCHAR(200) NOT NULL,
- valor NUMERIC (10, 2) NOT NULL,
- cod_tipo INT NOT NULL,
- CONSTRAINT fk_tipo_item FOREIGN KEY (cod_tipo) REFERENCES
- tb_tipo_item(cod_tipo)
- );
+CREATE TABLE IF NOT EXISTS tb_item(
+    cod_item SERIAL PRIMARY KEY,
+    descricao VARCHAR(200) NOT NULL,
+    valor NUMERIC (10, 2) NOT NULL,
+    cod_tipo INT NOT NULL,
+    CONSTRAINT fk_tipo_item FOREIGN KEY (cod_tipo) REFERENCES
+    tb_tipo_item(cod_tipo)
+);
 
-  INSERT INTO tb_item (descricao, valor, cod_tipo) VALUES
- ('Refrigerante', 7, 1), ('Suco', 8, 1), ('Hamburguer', 12, 2), ('Batata frita', 9, 2);
+INSERT INTO tb_item (descricao, valor, cod_tipo) VALUES
+('Refrigerante', 7, 1), ('Suco', 8, 1), ('Hamburguer', 12, 2), ('Batata frita', 9, 2);
 
  SELECT * FROM tb_item;
 
- CREATE TABLE IF NOT EXISTS tb_item_pedido(
- cod_item_pedido SERIAL PRIMARY KEY,
- cod_item INT,
- cod_pedido INT,
- CONSTRAINT fk_item FOREIGN KEY (cod_item) REFERENCES tb_item (cod_item),
- CONSTRAINT fk_pedido FOREIGN KEY (cod_pedido) REFERENCES tb_pedido
- (cod_pedido)
- );
+CREATE TABLE IF NOT EXISTS tb_item_pedido(
+    cod_item_pedido SERIAL PRIMARY KEY,
+    cod_item INT,
+    cod_pedido INT,
+    CONSTRAINT fk_item FOREIGN KEY (cod_item) REFERENCES tb_item (cod_item),
+    CONSTRAINT fk_pedido FOREIGN KEY (cod_pedido) REFERENCES tb_pedido
+    (cod_pedido)
+);
 
+CREATE OR REPLACE PROCEDURE sp_cadastrar_cliente (IN nome VARCHAR(200), IN
+ p_cod_cliente INT DEFAULT NULL)
+ LANGUAGE plpgsql
+ AS $$
+ BEGIN
+    IF p_cod_cliente IS NULL THEN
+    INSERT INTO tb_cliente (nome) VALUES (p_nome);
+    ELSE
+    INSERT INTO tb_cliente (cod_cliente, nome) VALUES (p_cod_cliente, p_nome);
+    END IF;
+ END;
+ $$
+
+ -- Inserção de um pedido sem itens
+
+CREATE OR REPLACE PROCEDURE sp_criar_pedido(
+    OUT p_cod_pedido INT, IN p_cod_cliente INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO tb_pedido(cod_cliente) VALUES (p_cod_cliente);
+    SELECT LASTVAL() INTO p_cod_pedido;
+END;
+$$
+
+INSERT INTO tb_cliente (nome) VALUES ('Ana Silva');
+SELECT * FROM tb_cliente;
+
+DO $$
+DECLARE
+    v_cod_pedido INT;
+    v_cod_cliente INT;
+BEGIN
+    SELECT c.cod_cliente FROM tb_cliente c 
+     WHERE nome LIKE 'Ana Silva' INTO v_cod_cliente;
+    CALL sp_criar_pedido(v_cod_pedido, v_cod_cliente); -- parametro que roda no modo OUT
+    RAISE NOTICE 'Código do pedido recém criado: %', v_cod_pedido; 
+END;
+$$
+
+SELECT * FROM tb_pedido;
+
+-- ADICIONA ITEM AO PEDIDO 
+-- liga o item ao pedido
+
+CREATE OR REPLACE PROCEDURE sp_adicionar_item_a_pedido(
+    IN p_cod_item INT, IN p_cod_pedido INT
+    )
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO tb_item_pedido(cod_item, cod_pedido)
+    VALUES($1, $2);
+    UPDATE tb_pedido p SET
+        data_modificacao = current_timestamp
+        WHERE p.cod_pedido = $2;
+END;
+$$
+
+CALL sp_adicionar_item_a_pedido(1, 8);
+select * from tb_item;
+
+CREATE OR REPLACE PROCEDURE sp_calcular_valor_de_um_pedido(
+    IN p_cod_pedido INT, OUT p_valor_total INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    SELECT SUM(i.valor) FROM 
+    tb_pedido p
+    INNER JOIN tb_item_pedido ip 
+    ON p.cod_pedido = ip.cod_pedido
+    INNER JOIN tb_item i 
+    ON ip.cod_item = i.cod_item
+    WHERE p.cod_pedido = p_cod_pedido
+    INTO $2;
+END;
+$$
